55214339c17aa70b5601b8623d907fc660ae4518
ittyi <vioiv.k4@gmail.com>
2025-03-03 22:31:26 +0900

    feat: 半角スペースでリスト化re.split(r'(\s)', text)

--- ch2/sample-read.py
+++ ch2/sample-read.py
@@ -1,5 +1,12 @@
+import re
+
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
     
 print("Total number of character:", len(raw_text))
 print(raw_text[:99])
+
+text = "Hello, world. This is a test"
+result = re.split(r'(\s)', text)
+print(result)
+# ['Hello,', ' ', 'world.', ' ', 'This', ' ', 'is', ' ', 'a', ' ', 'test']
