61288714699e7b3b94566245179075d4baf3be8d
ittyi <vioiv.k4@gmail.com>
2025-03-05 02:00:09 +0900

    🚀 feat: [ch2] preprocessed を定義し、raw_text をトークナイズ

--- ch2/sample-read.py
+++ ch2/sample-read.py
@@ -25,3 +25,13 @@ result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
 result = [item.strip() for item in result if item.strip()]
 print(result)
 # ['Hello', ',', 'world', '.', 'Is', 'this', '--', 'a', 'test', '?']
+
+preprocessed = re.split('([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(len(preprocessed))
+# 4690
+print(preprocessed[:30])
+# ['I', 'HAD', 'always', 'thought', 'Jack', 'Gisburn', 'rather', 
+# 'a', 'cheap', 'genius', '--', 'though', 'a', 'good', 'fellow', 
+# 'enough', '--', 'so', 'it', 'was', 'no', 'great', 'surprise', 
+# 'to', 'me', 'to', 'hear', 'that', ',', 'in']
