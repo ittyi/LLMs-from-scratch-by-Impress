0ec08f88fddbf1adc4f2478c91bc515ed03001ed
ittyi <vioiv.k4@gmail.com>
2025-03-05 01:55:27 +0900

    feat: [ch2] 疑問符、引用符、ダブルダッシュなどの特殊文字にも対応

--- ch2/sample-read.py
+++ ch2/sample-read.py
@@ -18,3 +18,8 @@ print(result)
 result = [item for item in result if item.strip()]
 print(result)
 # ['Hello', ',', 'world', '.', 'This', 'is', 'a', 'test']
+
+text = "Hello, world. Is this-- a test?"
+result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+print(result)
+# ['Hello', ',', '', ' ', 'world', '.', '', ' ', 'Is', ' ', 'this', '--', '', ' ', 'a', ' ', 'test', '?', '']
